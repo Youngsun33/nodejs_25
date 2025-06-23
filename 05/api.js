792e556a9698bd932ab4e6167b83b520f9360419
Youngsun33 <heoyoungsun33@gmail.com>
2025-06-23 14:41:14 +0900

    답글

--- 05/api.js
+++ 05/api.js
@@ -2,7 +2,7 @@ const express = require("express"); //익스프레스 모듈 임포트
 const moment = require("moment"); //날짜모듈 임포트
 const Database = require("better-sqlite3"); // sqlite 임포트
 const path = require("path"); //경로 모듈 임포트
-const { off } = require("process");
+const { prependListener } = require("process");
 
 //DB setting
 const db_name = path.join(__dirname, "post.db"); //sqlite용 데이터 베이스 파일 (소용량)
@@ -13,6 +13,12 @@ const app = express(); // 앱이란 변수에 익스프레스 함수를 담음/ 
 const PORT = 3000; //포트설정
 app.use(express.json()); //app.use는 미들웨어 설정. ㄴ모든 요청과 응답에 json 포멧을 사용함.
 
+//전체적인 라우터에 동작을 넣고 싶을때
+app.use((req, res, next) => {
+  console.log("나의 첫번째 미들웨어");
+  next();
+});
+
 //1. post.db에 게시판 전용 테이블을 만들기
 const create_sql = `
 CREATE TABLE IF NOT EXISTS posts (
@@ -22,8 +28,19 @@ CREATE TABLE IF NOT EXISTS posts (
     author VARCHAR(100),
     createdAt DATETIME ,
     count INTEGER DEFAULT 0
-
 );
+
+create table if not exists comments(
+    id integer primary key autoincrement,
+    content text,
+    author text,
+    createdAt DATETIME,
+    postId integer,
+    FOREIGN KEY(postId) REFERENCES posts(id) ON DELETE CASCADE
+
+    );
+
+
 `;
 
 db.exec(create_sql); //데이터베이스 만드는거 실행   //exec는 sql을 실행.
@@ -38,12 +55,15 @@ app.post("/posts", (req, res) => {
         values(?,?,?,?);
     `;
   const stmt = db.prepare(sql);
-  stmt.run(title, content, author, createdAt);
+  const result = stmt.run(title, content, author, createdAt); //insert into posts -> 자동증가된 id가 변환 :lastInsertRowid
+  const newPost = db
+    .prepare(`select * from posts where id =?`)
+    .get(result.lastInsertRowid);
   //stmi.run : updata, insert , delete
   //stmt.all : select * from  table or select * from table where -->[]배열로 값 반환(여러개니까)
   //stmt.get : select * from  table limt 1 --> {}객체로 값 변환
 
-  res.status(201).json({ message: "ok" });
+  res.status(201).json({ message: "ok", data: newPost });
 });
 
 //게시글 목록 가져오기
@@ -57,8 +77,21 @@ app.get("/posts", (req, res) => {
 
   const stmt = db.prepare(sql);
   const rows = stmt.all(limit, offset);
-  console.log(rows);
-  res.status(200).json({ data: rows });
+  //전체 게시글 수 조회
+  const totalCount = db
+    .prepare(`select count(*) as count from posts`)
+    .get().count;
+  const totalPages = Math.ceil(totalCount / limit);
+
+  res.status(200).json({
+    data: rows,
+    pagination: {
+      currentPage: page,
+      totalPages: totalPages,
+      totalCount: totalCount,
+      limit: limit,
+    },
+  });
 });
 
 //게시글 상세 조회
@@ -86,7 +119,12 @@ app.put("/posts/:id", (req, res) => {
   const stmi = db.prepare(sql);
   stmi.run(title, content, id);
   //res.redirect("/posts"); //redirec는 목록으로 나옴.게시글 목록 불러오는 저 위에 있는걸 니가 내부적으로 한번 더 호출해. GET http://localhost:3000/posts ->이거 내부적으로 한번 실행할라는 말
-  res.json({ message: "ok" });
+
+  const updatedPost = db.prepare(`select * from posts where id =?`).get(id);
+  if (!updatedPost) {
+    return res.status(404).json({ message: "게시물을 찾을 수 없습니다." });
+  }
+  res.status(200).json({ message: "ok", data: updatedPost });
 });
 
 //게시글 삭제
@@ -96,6 +134,82 @@ app.delete("/posts/:id", (req, res) => {
   const stmi = db.prepare(sql); //쿼리문 준비
   stmi.run(id); //실행
   res.json({ message: "ok" });
+});
+
+//댓글 추가
+
+app.post("/posts/:id/comments", (req, res) => {
+  const postId = req.params.id;
+  const { content, author } = req.body;
+  const createdAt = moment().format("YYYY-MM-DD HH:mm:ss");
+
+  const post = db.prepare(`select id from posts where id =?`).get(postId);
+  if (!post) {
+    return res.status(404).json({ message: "게시물을 찾을 수 없습니다." });
+  }
+  const sql = ` insert into comments(postId, author, content, createdAt) values (?,?,? ,?)`;
+  const result = db.prepare(sql).run(postId, author, content, createdAt);
+
+  const newComment = db
+    .prepare(`select * from comments where id =?`)
+    .get(result.lastInsertRowid);
+
+  res.status(200).json({ message: "ok", data: newComment });
+});
+
+// 답변 가져오기
+app.get("/posts/:id/comments", (req, res) => {
+  const postId = req.params.id;
+
+  const post = db.prepare(`select * from posts where id = ?`).get(postId);
+  if (!post) {
+    return res.status(404).json({ message: "게시물을 찾을 수 없습니다." });
+  }
+
+  const sql = `select id, author, content, createdAt from comments where postId = ? order by id desc`;
+  const comments = db.prepare(sql).all(postId);
+  res.status(200).json({ message: "ok", data: comments });
+});
+
+//답글 삭제
+app.delete("/posts/:postId/comments/:commentId", (req, res) => {
+  const { postId, commentId } = req.params;
+  const comments = db
+    .prepare(`select * from comments where postId =? and Id = ?`)
+    .get(postId, commentId);
+  if (!comments) {
+    return res.status(404).json({ message: "게시물을 찾을 수 없습니다." });
+  }
+
+  const sql = ` delete from comments where id =?`;
+  db.prepare(sql).run(commentId);
+  res.status(200).end();
+});
+
+//답글 부분 수정
+app.put("/posts/:postId/comments/:commentId", (req, res) => {
+  const { postId, commentId } = req.params;
+  const { author, content } = req.body;
+
+  const comment = db
+    .prepare(`select * from comments where postId =? and Id = ?`)
+    .get(postId, commentId);
+  if (!comment) {
+    return res.status(404).json({ message: "게시물을 찾을 수 없습니다." });
+  }
+
+  const newAuthor = author !== undefined ? author : comment.author;
+  const newContent = content !== undefined ? content : comment.content;
+
+  db.prepare(`update comments set author = ? , content =? where id =?`).run(
+    newAuthor,
+    newContent,
+    commentId
+  );
+  const updataComment = db
+    .prepare(`select * from comments where id =?`)
+    .get(commentId);
+  res.status(200).json({ message: "ok", data: updataComment });
 });
 
 //npx nodemon api.js
